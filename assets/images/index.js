5700352f2bfd227e682cd00a690df3232ba4ca89
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2023-08-29 04:55:14 +0530

    Added Icons and Splash Screen

--- assets/images/index.js
+++ assets/images/index.js
@@ -3,3 +3,4 @@ export const Squirtle = require("./Squirtle.png");
 export const Bulbasaur = require("./Bulbasaur.png");
 export const Ash = require("./Ash.png");
 export const Pokemon = require("./Pokemon.png");
+export const Pokeball = require("./Pokeball.png");
